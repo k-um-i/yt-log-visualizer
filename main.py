bda96f4f4c5d26d6aabb90455a57eb887941c79e
k-um-i <rsilen@proton.me>
2025-05-06 12:20:47 +0300

    main.py now functions based on command line arguments.

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import argparse
 from parser import parse_log
 from visualizer import render_overview
 
 
 def main():
-    log_data = parse_log("immersion_logs_269066190796750848.csv")
-    render_overview(log_data)
+    parser = argparse.ArgumentParser(
+        description="Generate Youtube log overview from CSV."
+    )
+    parser.add_argument("csv_file", help="Path to the CSV file to parse.")
+    parser.add_argument(
+        "-o", "--output", help="Specify output file path.", default="overview.html"
+    )
+
+    args = parser.parse_args()
+
+    log_data = parse_log(args.csv_file)
+    render_overview(log_data, output_file=args.output)
 
 
 if __name__ == "__main__":
